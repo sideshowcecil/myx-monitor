8acfeb707b97a2d7eacd63a95ef1f2ecb8b68da4
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-01-03 15:37:10 +0100

    updated interface javadoc

--- pubsub/src/main/java/at/ac/tuwien/infosys/pubsub/middleware/arch/interfaces/IRegistry.java
+++ pubsub/src/main/java/at/ac/tuwien/infosys/pubsub/middleware/arch/interfaces/IRegistry.java
@@ -4,12 +4,43 @@ import at.ac.tuwien.infosys.pubsub.middleware.arch.component.PublisherEndpoint;
 import at.ac.tuwien.infosys.pubsub.middleware.arch.component.SubscriberEndpoint;
 
 public interface IRegistry<E> {
-	
+	/**
+	 * Register a new {@link PublisherEndpoint} with the given topic, including
+	 * topic existence validation.
+	 * 
+	 * @param topic
+	 * @param publisher
+	 * @throws IllegalArgumentException
+	 */
 	public void register(String topic, PublisherEndpoint<E> publisher);
-	
-	public void unregister(String topic, PublisherEndpoint<E> publisher); 
 
+	/**
+	 * Unregister an existing {@link PublisherEndpoint} from the given topic,
+	 * including topic existence validation.
+	 * 
+	 * @param topic
+	 * @param publisher
+	 * @throws IllegalArgumentException
+	 */
+	public void unregister(String topic, PublisherEndpoint<E> publisher);
+
+	/**
+	 * Register a new {@link SubscriberEndpoint} with the given topic, including
+	 * topic existence validation.
+	 * 
+	 * @param topic
+	 * @param subscriber
+	 * @throws IllegalArgumentException
+	 */
 	public void register(String topic, SubscriberEndpoint<E> subscriber);
-	
+
+	/**
+	 * Unregister an existing {@link SubscriberEndpoint} from the given topic,
+	 * including topic existence validation.
+	 * 
+	 * @param topic
+	 * @param subscriber
+	 * @throws IllegalArgumentException
+	 */
 	public void unregister(String topic, SubscriberEndpoint<E> subscriber);
 }
--- pubsub/src/main/java/at/ac/tuwien/infosys/pubsub/middleware/arch/interfaces/ISubscriber.java
+++ pubsub/src/main/java/at/ac/tuwien/infosys/pubsub/middleware/arch/interfaces/ISubscriber.java
@@ -3,5 +3,10 @@ package at.ac.tuwien.infosys.pubsub.middleware.arch.interfaces;
 import at.ac.tuwien.infosys.pubsub.message.Message;
 
 public interface ISubscriber<E> {
+	/**
+	 * Send a message to the real subscriber.
+	 * 
+	 * @param message
+	 */
 	public void send(Message<E> message);
 }

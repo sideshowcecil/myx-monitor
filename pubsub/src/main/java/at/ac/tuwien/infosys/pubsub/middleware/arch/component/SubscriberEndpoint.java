54ef8ba1a5f74be5ae45cf7fe5e52da990e33cf4
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-01-03 13:37:47 +0100

    added first implementation of SubscriberEndpoint

--- pubsub/src/main/java/at/ac/tuwien/infosys/pubsub/middleware/arch/component/SubscriberEndpoint.java
+++ pubsub/src/main/java/at/ac/tuwien/infosys/pubsub/middleware/arch/component/SubscriberEndpoint.java
@@ -1,5 +1,11 @@
 package at.ac.tuwien.infosys.pubsub.middleware.arch.component;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import at.ac.tuwien.infosys.pubsub.message.Message;
 import at.ac.tuwien.infosys.pubsub.middleware.arch.interfaces.IDispatcher;
 import at.ac.tuwien.infosys.pubsub.middleware.arch.interfaces.IRegistry;
 import at.ac.tuwien.infosys.pubsub.middleware.arch.interfaces.ISubscriber;
@@ -20,6 +26,11 @@ public abstract class SubscriberEndpoint<E> extends AbstractMyxSimpleBrick
 	protected IDispatcher<E> _dispatcher;
 	protected IRegistry<E> _registry;
 
+	private ExecutorService _executor;
+	private Runnable _endpoint;
+	
+    private BlockingQueue<Message<E>> _queue = new LinkedBlockingQueue<>();
+
 	@Override
 	public Object getServiceObject(IMyxName arg0) {
 		// if no interfaces are going in, always return null
@@ -30,9 +41,37 @@ public abstract class SubscriberEndpoint<E> extends AbstractMyxSimpleBrick
 		return null;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void init() {
+		_executor = Executors.newSingleThreadExecutor();
+		_endpoint = new Runnable() {
+			public void run() {
+				// wait for the topic name
+				String topic = waitForTopicName();
+				// if we do not get a topic name we assume the publisher died
+				if (topic != null) {
+					// check if the topic exists
+					try {
+						_registry.register(topic, SubscriberEndpoint.this);
+					} catch (IllegalArgumentException ex) {
+						sendErrorForNonExistingTopic();
+						return;
+					}
+					// wait for messages and forward them to the subscribers
+					while (true) {
+						// wait for a message
+						Message<E> msg = _queue.poll();
+						// TODO: check for CLOSE or ERROR messages so we may shut down the endpoint.
+						// TODO: maybe don't use a queue here but rather forward it directly to the real endpoint
+					}
+				}
+			}
+		};
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void begin() {
 		try {
 			// connect interfaces
 			_dispatcher = (IDispatcher<E>) getFirstRequiredServiceObject(OUT_IDISPATCHER);
@@ -41,6 +80,24 @@ public abstract class SubscriberEndpoint<E> extends AbstractMyxSimpleBrick
 			System.err.println(ex.getMessage());
 			return;
 		}
+		_executor.execute(_endpoint);
 	}
+	
+	@Override
+	public void send(Message<E> message) {
+		_queue.offer(message);
+	}
+
+    /**
+     * Wait and return the topic name used by this subscriber.
+     * 
+     * @return
+     */
+    public abstract String waitForTopicName();
+
+    /**
+     * Send an error to the subscriber if the topic name is not registered.
+     */
+    public abstract void sendErrorForNonExistingTopic();
 
 }

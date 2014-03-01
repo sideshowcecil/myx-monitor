484092c39cdbbce8805dd25302addde3fd5a63fd
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-03-01 11:52:16 +0100

    updated MessageDistributor to use a fixed thread pool

--- pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/middleware/arch/myx/MessageDistributor.java
+++ pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/middleware/arch/myx/MessageDistributor.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import at.ac.tuwien.dsg.pubsub.message.Message;
 import at.ac.tuwien.dsg.pubsub.message.Message.Type;
@@ -23,6 +24,13 @@ import edu.uci.isr.myx.fw.IMyxName;
 public class MessageDistributor extends EventPumpConnector {
 
     private List<Tuple<Method, Object[]>> initCalls = new ArrayList<>();
+    
+    @Override
+    public void init() {
+        super.init();
+        // here we use a fixed thread pool to increase the throughput
+        asyncExecutor = Executors.newFixedThreadPool(2);
+    }
 
     @Override
     public void interfaceConnected(IMyxName interfaceName, Object serviceObject) {

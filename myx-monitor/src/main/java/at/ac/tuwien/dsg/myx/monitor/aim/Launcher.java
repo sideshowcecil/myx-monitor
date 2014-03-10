26015d93aeac9d1738780c1c82d3cc583c84a46f
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-03-10 14:28:25 +0100

    updated javadoc

--- myx-monitor/src/main/java/at/ac/tuwien/dsg/myx/monitor/aim/Launcher.java
+++ myx-monitor/src/main/java/at/ac/tuwien/dsg/myx/monitor/aim/Launcher.java
@@ -3,5 +3,12 @@ package at.ac.tuwien.dsg.myx.monitor.aim;
 import edu.uci.isr.xarch.types.IArchStructure;
 
 public interface Launcher {
+    /**
+     * Instantiat the given {@link IArchStructure} and run it.
+     * 
+     * @param name
+     * @param structure
+     * @throws ArchitectureInstantiationException
+     */
     public void instantiate(String name, IArchStructure structure) throws ArchitectureInstantiationException;
 }

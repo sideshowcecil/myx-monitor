db47f1dc7bd336b694b00603decbf14bc86f8b04
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-03-13 17:52:22 +0100

    added needed MyxProperties that contains the names of the keys used to get architecture information out of the init properties

---
+++ myx-monitor/src/main/java/at/ac/tuwien/dsg/myx/monitor/MyxProperties.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.dsg.myx.monitor;
+
+public interface MyxProperties {
+    public static String XADL_FILE = "file";
+    public static String STRUCTURE_NAME = "structure";
+    public static String ARCHITECTURE_RUNTIME_ID = "architectureRuntimeId";
+    public static String ARCHITECTURE_BLUEPRINT_ID = "architectureBlueprintId";
+    public static String ARCHITECTURE_BRICK_TYPE = "architectureBrickType";
+    public static String ARCHITECTURE_INTERFACE_TYPE = "architectureInterfaceType";
+}

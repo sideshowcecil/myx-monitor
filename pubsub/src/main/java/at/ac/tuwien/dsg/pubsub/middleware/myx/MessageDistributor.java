eba04ba54579139d5b98054f9428e4abe19c77da
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-10-04 14:07:12 +0200

    changes the MessageDistributor as it is no longer a subclass of EventPumpConnector and introduces some enhancements

--- pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/middleware/myx/MessageDistributor.java
+++ pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/middleware/myx/MessageDistributor.java
@@ -1,32 +1,112 @@
 package at.ac.tuwien.dsg.pubsub.middleware.myx;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import at.ac.tuwien.dsg.myx.util.MyxMonitoringUtils;
 import at.ac.tuwien.dsg.myx.util.Tuple;
 import at.ac.tuwien.dsg.pubsub.message.Message;
 import at.ac.tuwien.dsg.pubsub.message.Message.Type;
-import edu.uci.isr.myx.conn.EventPumpConnector;
+import edu.uci.isr.myx.fw.AbstractMyxSimpleBrick;
+import edu.uci.isr.myx.fw.IMyxClassManager;
+import edu.uci.isr.myx.fw.IMyxDynamicBrick;
+import edu.uci.isr.myx.fw.IMyxInterfaceDescription;
 import edu.uci.isr.myx.fw.IMyxName;
+import edu.uci.isr.myx.fw.MyxJavaClassInterfaceDescription;
+import edu.uci.isr.myx.fw.MyxUtils;
 
 /**
- * An extension of the EventPumpConnector that allows to save special calls that
- * are sent to every connected interface.
+ * An alternative implementation of the EventPumpConnector that allows to save
+ * special calls that are sent to every connected interface.
  * 
  * @author bernd.rathmanner
  * 
  */
-public class MessageDistributor extends EventPumpConnector {
+public class MessageDistributor extends AbstractMyxSimpleBrick implements IMyxDynamicBrick, InvocationHandler {
 
     private Map<String, List<Tuple<Method, Object[]>>> initCalls = new HashMap<>();
+
+    public static final IMyxName REQUIRED_INTERFACE_NAME = MyxMonitoringUtils.createName("out");
+    public static final IMyxName PROVIDED_INTERFACE_NAME = MyxMonitoringUtils.createName("in");
+
+    protected List<Object> trueServiceObjects = new CopyOnWriteArrayList<>();
+    protected Object proxyObject = null;
+
+    protected ExecutorService executor = null;
+
+    @Override
+    public void init() {
+        Set<String> interfaceClassNames = new HashSet<String>();
+
+        IMyxInterfaceDescription miDesc = getMyxBrickItems().getInterfaceManager().getInterfaceDescription(
+                PROVIDED_INTERFACE_NAME);
+        if (miDesc instanceof MyxJavaClassInterfaceDescription) {
+            MyxJavaClassInterfaceDescription jmiDesc = (MyxJavaClassInterfaceDescription) miDesc;
+            interfaceClassNames.addAll(Arrays.asList(jmiDesc.getServiceObjectInterfaceNames()));
+        }
+
+        int i = 0;
+        while (true) {
+            String interfaceClassName = MyxUtils.getInitProperties(this).getProperty("interfaceClassName" + i);
+            if (interfaceClassName == null)
+                break;
+            interfaceClassNames.add(interfaceClassName);
+            i++;
+        }
+
+        List<Class<?>> interfaceClassList = new ArrayList<Class<?>>();
+        IMyxClassManager classManager = getMyxBrickItems().getClassManager();
+        for (String interfaceClassName : interfaceClassNames) {
+            try {
+                Class<?> interfaceClass = classManager.classForName(interfaceClassName);
+                interfaceClassList.add(interfaceClass);
+            } catch (ClassNotFoundException cnfe) {
+                throw new IllegalArgumentException("Can't find interface class: " + cnfe.getMessage());
+            }
+        }
+
+        Class<?>[] interfaceClasses = interfaceClassList.toArray(new Class[interfaceClassList.size()]);
+        if (interfaceClasses.length > 0) {
+            proxyObject = Proxy.newProxyInstance(interfaceClasses[0].getClassLoader(), interfaceClasses, this);
+        }
+        executor = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public void end() {
+        if (executor != null) {
+            executor.shutdown();
+            try {
+                executor.awaitTermination(5L, TimeUnit.SECONDS);
+            } catch (InterruptedException ie) {
+            }
+        }
+    }
 
     @Override
     public void interfaceConnected(IMyxName interfaceName, Object serviceObject) {
         if (interfaceName.equals(REQUIRED_INTERFACE_NAME)) {
+            trueServiceObjects.add(serviceObject);
+
+            if (proxyObject == null) {
+                ClassLoader cl = serviceObject.getClass().getClassLoader();
+                Class<?>[] interfaceClasses = serviceObject.getClass().getInterfaces();
+                proxyObject = Proxy.newProxyInstance(cl, interfaceClasses, this);
+            }
+
             if (initCalls.size() > 0) {
                 final Object tso = serviceObject;
                 for (List<Tuple<Method, Object[]>> calls : initCalls.values()) {
@@ -46,18 +126,45 @@ public class MessageDistributor extends EventPumpConnector {
                                 }
                             }
                         };
-                        asyncExecutor.execute(r);
+                        executor.execute(r);
                     }
                 }
             }
         }
-        super.interfaceConnected(interfaceName, serviceObject);
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public void interfaceDisconnecting(IMyxName interfaceName, Object serviceObject) {
+        if (interfaceName.equals(REQUIRED_INTERFACE_NAME)) {
+            trueServiceObjects.remove(serviceObject);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void interfaceDisconnected(IMyxName interfaceName, Object serviceObject) {
+    }
+
+    @Override
+    public Object getServiceObject(IMyxName interfaceName) {
+        if (interfaceName.equals(PROVIDED_INTERFACE_NAME)) {
+            return proxyObject;
+        }
+        return null;
+    }
+
+    @Override
+    public Object invoke(@SuppressWarnings("unused") Object proxy, Method method, Object[] args) throws Throwable {
+        if (proxyObject == null) {
+            // asynchronous messages do not have to get delivered.
+            return null;
+        }
+        if (trueServiceObjects.isEmpty()) {
+            // asynchronous messages do not have to get delivered.
+            return null;
+        }
+
         // if the given arguments contain message objects we search for
-        // init-messages
+        // application specific init-messages
         for (Object object : args) {
             if (object instanceof Message<?>) {
                 Message<?> msg = (Message<?>) object;
@@ -71,7 +178,27 @@ public class MessageDistributor extends EventPumpConnector {
                 }
             }
         }
-        return super.invoke(proxy, method, args);
-    }
 
+        for (final Object serviceObject : trueServiceObjects) {
+            final Method m = method;
+            final Object[] a = args;
+            Runnable r = new Runnable() {
+                public void run() {
+                    try {
+                        m.invoke(serviceObject, a);
+                    } catch (IllegalAccessException iae) {
+                        iae.printStackTrace();
+                        return;
+                    } catch (InvocationTargetException ite) {
+                        ite.printStackTrace();
+                        return;
+                    }
+                }
+            };
+
+            executor.execute(r);
+        }
+        // we don't return values from asynchronous calls
+        return null;
+    }
 }

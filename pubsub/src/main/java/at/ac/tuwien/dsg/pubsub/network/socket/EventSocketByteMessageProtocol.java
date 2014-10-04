903a8b7d4ad25c7a3ddb7b862a7a10a67680f2a2
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-10-04 16:47:46 +0200

    increases the performance of the network procotol implementations

--- pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/network/socket/EventSocketByteMessageProtocol.java
+++ pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/network/socket/EventSocketByteMessageProtocol.java
@@ -1,14 +1,16 @@
 package at.ac.tuwien.dsg.pubsub.network.socket;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 import org.apache.commons.codec.binary.Base64;
@@ -32,20 +34,24 @@ public final class EventSocketByteMessageProtocol implements Endpoint<Event> {
     public static final char SEPARATOR = ';';
 
     private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
 
     /**
      * Default constructor.
      * 
      * @throws IOException
      */
-    public EventSocketByteMessageProtocol(Socket socket) {
+    public EventSocketByteMessageProtocol(Socket socket) throws IOException {
         this.socket = socket;
+        this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        this.out = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
     }
 
     @Override
     public void send(Message<Event> msg) throws IOException {
         // serialize the object
-        byte[] serialized = null;
+        String serialized = null;
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectOutput oo = null;
@@ -53,7 +59,7 @@ public final class EventSocketByteMessageProtocol implements Endpoint<Event> {
             oo = new ObjectOutputStream(bos);
             oo.writeObject(msg.getData());
             oo.close();
-            serialized = Base64.encodeBase64String(bos.toByteArray()).getBytes();
+            serialized = Base64.encodeBase64String(bos.toByteArray());
         } catch (IOException e) {
         } finally {
             try {
@@ -62,12 +68,11 @@ public final class EventSocketByteMessageProtocol implements Endpoint<Event> {
             } catch (Exception e) {
             }
         }
-        
-        // write to the stream        
-        OutputStream out = socket.getOutputStream();
-        out.write(msg.getType().toString().getBytes());
+
+        // write to the stream
+        out.write(msg.getType().toString());
         out.write(SEPARATOR);
-        out.write(msg.getTopic().getBytes());
+        out.write(msg.getTopic());
         out.write(SEPARATOR);
         out.write(serialized);
         out.write(CR);
@@ -77,59 +82,28 @@ public final class EventSocketByteMessageProtocol implements Endpoint<Event> {
 
     @Override
     public Message<Event> receive() throws IOException {
-        Type type = null;
-        String topic = null;
-        StringBuilder sb = new StringBuilder();
-        
-        // read the message
-        char last = NULL;
-        InputStream in = socket.getInputStream();
-        while (true) {
-            int b = in.read();
-            // check if a byte is available
-            if (b == -1) {
-                break;
-            }
+        String line = in.readLine();
+        if (line == null) {
+            // the end of stream was reached, thus we return a close message
+            return new Message<Event>(Type.CLOSE, "", null);
+        }
+        String[] parts = line.split(SEPARATOR + "");
+        if (parts.length != 3) {
+            return new Message<Event>(Type.ERROR, "", null);
+        }
 
-            char c = (char) b;
-            if (c == SEPARATOR) {
-                // we caught the end of the message
-                if (type == null) {
-                    // the type has to be filled first
-                    try {
-                        type = Type.valueOf(sb.toString());
-                    } catch (IllegalArgumentException e) {
-                        // the type cannot be resolved
-                        type = Type.ERROR;
-                    }
-                } else if (topic == null) {
-                    // after this we fill the topic
-                    topic = sb.toString();
-                } else {
-                    // this should not have happened
-                }
-                sb = new StringBuilder();
-            } else {
-                // read all characters until the end of the message
-                if (c == CR && last == NULL) {
-                    last = c;
-                } else if (last == CR && c == LF) {
-                    sb.setLength(sb.length() - 1);
-                    break;
-                }
-                sb.append(c);
-            }
-        }
-        if (type == null || topic == null) {
+        Type type = null;
+        try {
+            type = Type.valueOf(parts[0]);
+        } catch (IllegalArgumentException e) {
+            // the type cannot be resolved
             type = Type.ERROR;
-            topic = "";
         }
-        
+
         // unserialize the object
         Event event = null;
-        if (sb.length() > 0) {
-            ByteArrayInputStream bis = new ByteArrayInputStream(
-                    Base64.decodeBase64(sb.toString()));
+        if (parts[2].length() > 0) {
+            ByteArrayInputStream bis = new ByteArrayInputStream(Base64.decodeBase64(parts[2]));
             ObjectInput oi = null;
             try {
                 oi = new ObjectInputStream(bis);
@@ -146,8 +120,8 @@ public final class EventSocketByteMessageProtocol implements Endpoint<Event> {
                 }
             }
         }
-        
-        return new Message<Event>(type, topic, event);
+
+        return new Message<Event>(type, parts[1], event);
     }
 
     @Override
--- pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/network/socket/SocketByteMessageProtocol.java
+++ pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/network/socket/SocketByteMessageProtocol.java
@@ -1,8 +1,10 @@
 package at.ac.tuwien.dsg.pubsub.network.socket;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 import org.apache.commons.codec.binary.Base64;
@@ -31,24 +33,27 @@ public final class SocketByteMessageProtocol implements Endpoint<byte[]> {
     public static final char ERROR = '-';
 
     private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
 
     /**
      * Default constructor.
      * 
      * @throws IOException
      */
-    public SocketByteMessageProtocol(Socket socket) {
+    public SocketByteMessageProtocol(Socket socket) throws IOException {
         this.socket = socket;
+        this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        this.out = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
     }
 
     @Override
     public void send(Message<byte[]> msg) throws IOException {
-        OutputStream out = socket.getOutputStream();
-        out.write(msg.getType().toString().getBytes());
+        out.write(msg.getType().toString());
         out.write(SEPARATOR);
-        out.write(msg.getTopic().getBytes());
+        out.write(msg.getTopic());
         out.write(SEPARATOR);
-        out.write(Base64.encodeBase64(msg.getData()));
+        out.write(Base64.encodeBase64String(msg.getData()));
         out.write(CR);
         out.write(LF);
         out.flush();
@@ -56,58 +61,39 @@ public final class SocketByteMessageProtocol implements Endpoint<byte[]> {
 
     @Override
     public Message<byte[]> receive() throws IOException {
+        String line = in.readLine();
+        if (line == null) {
+            // the end of stream was reached, thus we return a close message
+            return new Message<byte[]>(Type.CLOSE, "", new byte[0]);
+        }
+        String[] parts = line.split(SEPARATOR + "");
+        String data;
+        if (parts.length == 2 && line.endsWith(SEPARATOR + "")) {
+            // if no data is given the part is not contained, thus we have to
+            // fill it manually
+            data = "";
+        } else if (parts.length < 3) {
+            // not all parts are available
+            return new Message<byte[]>(Type.ERROR, "", new byte[0]);
+        } else {
+            data = parts[2];
+        }
+
         Type type = null;
-        String topic = null;
-
-        StringBuilder sb = new StringBuilder();
-        char last = NULL;
-        InputStream in = socket.getInputStream();
-        while (true) {
-            int b = in.read();
-            // check if a byte is available
-            if (b == -1) {
-                break;
-            }
-
-            char c = (char) b;
-            if (c == SEPARATOR) {
-                // we caught the end of the message
-                if (type == null) {
-                    // the type has to be filled first
-                    try {
-                        type = Type.valueOf(sb.toString());
-                    } catch (IllegalArgumentException e) {
-                        // the type cannot be resolved
-                        type = Type.ERROR;
-                    }
-                } else if (topic == null) {
-                    // after this we fill the topic
-                    topic = sb.toString();
-                } else {
-                    // this should not have happened
-                }
-                sb = new StringBuilder();
-            } else {
-                // read all characters until the end of the message
-                if (c == CR && last == NULL) {
-                    last = c;
-                } else if (last == CR && c == LF) {
-                    sb.setLength(sb.length() - 1);
-                    break;
-                }
-                sb.append(c);
-            }
-        }
-        if (type == null || topic == null) {
+        try {
+            type = Type.valueOf(parts[0]);
+        } catch (IllegalArgumentException e) {
+            // the type cannot be resolved
             type = Type.ERROR;
-            topic = "";
         }
-        return new Message<byte[]>(type, topic, Base64.decodeBase64(sb.toString().getBytes()));
+        return new Message<byte[]>(type, parts[1], Base64.decodeBase64(data));
     }
 
     @Override
     public void close() {
         try {
+            in.close();
+            out.close();
             socket.close();
         } catch (IOException e) {
         }

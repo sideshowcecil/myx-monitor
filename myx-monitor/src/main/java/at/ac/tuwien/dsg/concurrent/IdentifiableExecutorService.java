5a513478377a90f193da7f5888029ca3abd72ce6
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2014-10-29 17:16:35 +0100

    updates the javadoc of IdentifiableExecutorService

--- myx-monitor/src/main/java/at/ac/tuwien/dsg/concurrent/IdentifiableExecutorService.java
+++ myx-monitor/src/main/java/at/ac/tuwien/dsg/concurrent/IdentifiableExecutorService.java
@@ -4,17 +4,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
 
 /**
- * An extension to the {@link ExecutorService} interface that allow to execute
- * tasks on specific {@link Thread}s using identifiers.
- * 
- * @author bernd.rathmanner
- * 
+ * An extension to the {@link ExecutorService} interface that allow to 
+ * execute tasks on specific {@link Thread}s using identifiers.
  */
 public interface IdentifiableExecutorService extends ExecutorService {
     /**
      * Executes the given command at some time in the future. The command may
-     * execute in a new thread, in a pooled thread, or in the calling thread, at
-     * the discretion of the {@code Executor} implementation.
+     * execute in a new thread, in a pooled thread, or in the calling thread, 
+     * at the discretion of the {@code Executor} implementation. This method
+     * guarantees that a {@link Runnable} with the same identifier is always
+     * executed by the same {@link Thread}.
      * 
      * @param command
      *            the runnable task

bc0b4b986e62609af0dedee4da537de358e0d400
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2015-12-01 16:47:57 +0100

    changes the handling of the given arch structure

--- myx-monitor/src/main/java/at/ac/tuwien/dsg/myx/monitor/comp/BootstrapComponent.java
+++ myx-monitor/src/main/java/at/ac/tuwien/dsg/myx/monitor/comp/BootstrapComponent.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.dsg.myx.monitor.comp;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import at.ac.tuwien.dsg.myx.monitor.MyxProperties;
 import at.ac.tuwien.dsg.myx.monitor.aim.ArchitectureInstantiationException;
 import at.ac.tuwien.dsg.myx.monitor.aim.Launcher;
@@ -9,9 +12,6 @@ import at.ac.tuwien.dsg.myx.util.MyxUtils;
 import edu.uci.isr.myx.fw.AbstractMyxSimpleBrick;
 import edu.uci.isr.myx.fw.IMyxName;
 import edu.uci.isr.xarch.types.IArchStructure;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class BootstrapComponent extends AbstractMyxSimpleBrick {
 
@@ -33,24 +33,31 @@ public class BootstrapComponent extends AbstractMyxSimpleBrick {
         launcher = MyxUtils.getFirstRequiredServiceObject(this, INTERFACE_NAME_OUT_LAUNCHER);
         modelRoot = MyxUtils.getFirstRequiredServiceObject(this, INTERFACE_NAME_OUT_MODELROOT);
 
-        String structureDescription = MyxUtils.getInitProperties(this).getProperty(
-                MyxProperties.STRUCTURE_NAME, MyxProperties.DEFAULT_STRUCTURE_NAME);
-
+        // structure to be instantiated
         IArchStructure archStructure = null;
+        // extract arch structures
+        Collection<IArchStructure> archStructures = DBLUtils.getArchStructures(modelRoot.getArchitectureRoot());
 
-        List<IArchStructure> archStructures = new ArrayList<>();
-        for (IArchStructure as : DBLUtils.getArchStructures(modelRoot.getArchitectureRoot())) {
-            archStructures.add(as);
-            if (as.getDescription() != null && as.getDescription().getValue().equals(structureDescription)) {
-                archStructure = as;
-                break;
+        // which archstructe should be instantiated?
+        String structureDescription = MyxUtils.getInitProperties(this).getProperty(MyxProperties.STRUCTURE_NAME);
+        if (structureDescription != null) {
+            // extract a certain archstructure
+            for (IArchStructure as : archStructures) {
+                if (as.getDescription() != null && as.getDescription().getValue().equals(structureDescription)) {
+                    archStructure = as;
+                    break;
+                }
+            }
+        } else {
+            // use the first one if we got one
+            Iterator<IArchStructure> it = archStructures.iterator();
+            if (it.hasNext()) {
+                archStructure = it.next();
             }
         }
-        if (archStructures.isEmpty()) {
-            throw new RuntimeException("Architecture has no structures to instantiate!");
-        }
+
         if (archStructure == null) {
-            archStructure = archStructures.get(0);
+            throw new RuntimeException("No suitable structure found!");
         }
 
         try {
@@ -61,5 +68,4 @@ public class BootstrapComponent extends AbstractMyxSimpleBrick {
             System.exit(-1);
         }
     }
-
 }

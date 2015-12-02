3c1974927b14d29f7711b1d4fef1dac88ef511c6
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2015-12-02 19:11:40 +0100

    updates the parent class of MessageCreator

--- pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/publisher/comp/MessageCreator.java
+++ pubsub/src/main/java/at/ac/tuwien/dsg/pubsub/publisher/comp/MessageCreator.java
@@ -1,12 +1,12 @@
 package at.ac.tuwien.dsg.pubsub.publisher.comp;
 
-import edu.uci.isr.myx.fw.IMyxName;
-import at.ac.tuwien.dsg.myx.monitor.AbstractMyxExternalConnectionBrick;
 import at.ac.tuwien.dsg.myx.util.MyxUtils;
 import at.ac.tuwien.dsg.pubsub.middleware.interfaces.IPublisher;
 import at.ac.tuwien.dsg.pubsub.publisher.myx.MyxInterfaceNames;
+import edu.uci.isr.myx.fw.AbstractMyxSimpleBrick;
+import edu.uci.isr.myx.fw.IMyxName;
 
-public abstract class MessageCreator<E> extends AbstractMyxExternalConnectionBrick {
+public abstract class MessageCreator<E> extends AbstractMyxSimpleBrick {
 
     public static final IMyxName OUT_PUBLISHER = MyxInterfaceNames.IPUBLISHER;
 
